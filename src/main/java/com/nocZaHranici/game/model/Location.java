97ee75e52196c2348b3129b1ee2217420e1648ba
janvesely <vesjan007@gmail.com>
2026-02-15 13:16:34 +0100

    Added model, dto class to make basic logic and data variables for Location (id, name, description, items, npcs, exits)

---
+++ src/main/java/com/nocZaHranici/game/model/Location.java
@@ -0,0 +1,76 @@
+package com.nocZaHranici.game.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Location {
+    private String id;
+    private String name;
+    private String description;
+    private List<Item> items;
+    private List<NPC> npcs;
+    private Map<String, Location> exits;
+
+    public Location(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.items = new ArrayList<>();
+        this.npcs = new ArrayList<>();
+        this.exits = new HashMap<>();
+    }
+
+    // region get, set
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<NPC> getNpcs() {
+        return npcs;
+    }
+
+    public void setNpcs(List<NPC> npcs) {
+        this.npcs = npcs;
+    }
+
+    public Map<String, Location> getExits() {
+        return exits;
+    }
+
+    public void setExits(Map<String, Location> exits) {
+        this.exits = exits;
+    }
+
+    // endregion
+}
