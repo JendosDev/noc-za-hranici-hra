f9b2c142d433285aaf90ad227d2bafa8fa7f74c1
janvesely <vesjan007@gmail.com>
2026-02-18 15:36:41 +0100

    Added comments

--- src/main/java/com/nocZaHranici/game/command/GoCommand.java
+++ src/main/java/com/nocZaHranici/game/command/GoCommand.java
@@ -5,15 +5,35 @@ import com.nocZaHranici.game.model.GameWorld;
 import com.nocZaHranici.game.model.Location;
 import com.nocZaHranici.game.model.Player;
 
+/**
+ * Třída reprezentující příkaz pro pohyb hráče
+ */
 public class GoCommand implements Command {
+    /**
+     * Herní svět
+     */
     private GameWorld world;
+
+    /**
+     * Hráč
+     */
     private Player player;
 
+    /**
+     * Vytvoří instanci příkazu pro pohyb hráče
+     * @param world Herní svět
+     * @param player Hráč
+     */
     public GoCommand(GameWorld world, Player player) {
         this.world = world;
         this.player = player;
     }
 
+    /**
+     * Metoda zaznamenává aktuální lokaci hráče
+     * Po přechodu hráče přenastaví aktuální lokaci
+     * @param direction Směřovaná lokace
+     */
     @Override
     public void execute(String direction) {
         Location currentLocation = world.getLocation(player.getCurrentLocationId());
