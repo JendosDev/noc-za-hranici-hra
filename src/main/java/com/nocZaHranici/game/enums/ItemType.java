0c8a930027f6ee82082d0ca91fcccfedcb4cf921
janvesely <vesjan007@gmail.com>
2026-02-22 12:42:22 +0100

    Added all item types

--- src/main/java/com/nocZaHranici/game/enums/ItemType.java
+++ src/main/java/com/nocZaHranici/game/enums/ItemType.java
@@ -1,5 +1,11 @@
 package com.nocZaHranici.game.enums;
 
 public enum ItemType {
-
+    RUSTY_KEY,
+    NOTEBOOK,
+    TORCH,
+    BLOODY_AMULET,
+    MOUNTAIN_TALISMAN,
+    ROPE,
+    BOUNDARY_DUST
 }
