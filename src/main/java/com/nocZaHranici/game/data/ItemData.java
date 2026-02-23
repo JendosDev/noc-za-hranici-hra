7d5af9f0c3cdba3d6ab3427ca2801670a118f811
janvesely <vesjan007@gmail.com>
2026-02-23 14:07:15 +0100

    new variables (type,durability) to get all needed data

--- src/main/java/com/nocZaHranici/game/data/ItemData.java
+++ src/main/java/com/nocZaHranici/game/data/ItemData.java
@@ -1,5 +1,7 @@
 package com.nocZaHranici.game.data;
 
+import com.nocZaHranici.game.enums.ItemType;
+
 /**
  * Tčída reprezentující předměty v souboru
  */
@@ -20,19 +22,32 @@ public class ItemData {
      * Použitelnost
      */
     private boolean usable;
+    /**
+     * Typ
+     */
+    private String type;
+    /**
+     * Trvanlivost
+     */
+    private int durability;
 
     /**
-     * Vytvoří novou instanci předměty
+     * Vytvoří novou instanci předměty v souboru
      * @param id Id
      * @param name Jméno
      * @param description Popis
      * @param usable Použitelnost
+     * @param type Typ
+     * @param durability Trvanlivost
      */
-    public ItemData(String id, String name, String description, boolean usable) {
+    public ItemData(String id, String name, String description,
+                    boolean usable, String type, int durability) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.usable = usable;
+        this.type = type;
+        this.durability = durability;
     }
 
     public ItemData() {
@@ -71,6 +86,22 @@ public class ItemData {
 
     public void setUsable(boolean usable) {
         this.usable = usable;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
     }
 
     // endregion
