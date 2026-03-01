e603a9177a3051aa1e0cb06edd30273c869c3714
janvesely <vesjan007@gmail.com>
2026-03-01 21:47:53 +0100

    Updated - count of player health and attack - method: hasItem(): controls if player has a item in inventory

--- src/main/java/com/nocZaHranici/game/model/Player.java
+++ src/main/java/com/nocZaHranici/game/model/Player.java
@@ -1,8 +1,7 @@
 package com.nocZaHranici.game.model;
 
 import com.nocZaHranici.game.Inventory;
-import com.nocZaHranici.game.QuestState;
-import com.nocZaHranici.game.data.QuestData;
+import com.nocZaHranici.game.enums.QuestState;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,7 +9,10 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Třída reprezentující hráče ve hře
+ * Třída reprezentující hráče ve hře.
+ * Obsahuje informace o zdraví, útoku a inventáři.
+ *
+ * @author Jan Karel Vesely
  */
 public class Player {
 
@@ -70,6 +72,10 @@ public class Player {
      * Umrtí
      */
     private boolean dead;
+    /**
+     * Maximální životy
+     */
+    private int maxHealth;
 
     /**
      * Vytvoří instanci hráče
@@ -79,13 +85,20 @@ public class Player {
         this.inventory = new Inventory();
         this.quests = new HashMap<>();
 
-        this.health = 100;
-        this.attack = 10;
+        this.health = 200;
+        this.attack = 15;
 
         this.notebookRead = false;
         this.amuletEquipped = false;
         this.mountainProtection = false;
         this.lightSource = false;
+    }
+
+    public Player(String currentLocationId, int health, int attack) {
+        this.currentLocationId = currentLocationId;
+
+        this.health = health;
+        this.attack = attack;
     }
 
     public String getCurrentLocationId() {
@@ -140,11 +153,12 @@ public class Player {
     public void heal(int amount) {
         health += amount;
 
-        if (health > 100) {
-            health = 100;
+        if (health > 200) {
+            health = 200;
         }
     }
 
+    // region get, set
     public boolean isAlive() {
         return health > 0;
     }
@@ -197,6 +211,36 @@ public class Player {
         this.dead = dead;
     }
 
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public void setQuests(Map<String, QuestState> quests) {
+        this.quests = quests;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isMountainProtection() {
+        return mountainProtection;
+    }
+
+    public boolean isLightSource() {
+        return lightSource;
+    }
+
+    public boolean isEscaped() {
+        return escaped;
+    }
+
+    // endregion
+
     @Override
     public String toString() {
         return "Player{" +
@@ -208,5 +252,32 @@ public class Player {
                 ", mountainProtection=" + mountainProtection +
                 ", lightSource=" + lightSource +
                 '}';
+    }
+
+    /**
+     * Zkontroluje, zda hráč vlastní předmět podle ID.
+     *
+     * @param itemId ID předmětu
+     * @return true pokud předmět má, jinak false
+     */
+    /**
+     * Zkontroluje, zda hráč vlastní předmět podle ID.
+     *
+     * @param itemId ID předmětu
+     * @return true pokud hráč předmět má, jinak false
+     */
+    public boolean hasItem(String itemId) {
+
+        if (itemId == null) {
+            return false;
+        }
+
+        for (Item item : inventory.getItems()) {
+            if (item.getId().equals(itemId)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
